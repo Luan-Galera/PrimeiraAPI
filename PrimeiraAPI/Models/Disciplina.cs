344460b38f3d3be1e2e252daef70f847ab66f873
Luan-Galera <luangalera17@gmail.com>
2026-02-27 15:42:11 -0300

    Adiciona entidades Disciplina e DisciplinaCurso ao projeto
    Inclui DbSet para Disciplina e DisciplinaCurso no AppDbContext.
    Cria modelos Disciplina e DisciplinaCurso, incluindo propriedades
    e relacionamentos muitos-para-muitos com Curso.

--- PrimeiraAPI/Models/Disciplina.cs
+++ PrimeiraAPI/Models/Disciplina.cs
@@ -4,5 +4,12 @@ namespace PrimeiraAPI.Models
 {
     public class Disciplina
     {
+        public Guid DisciplinaId { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public int SemestreSugerido { get; set; }
+
+        public ICollection<Curso> Cursos = new List<Curso>();
     }
 }
---
+++ PrimeiraAPI/Models/DisciplinaCurso.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeiraAPI.Models
+{
+    public class DisciplinaCurso
+    {
+        public Guid DisciplinaCursoId { get; set; }
+
+        public Guid CursoID { get; set; }
+        public Curso? Curso { get; set; }
+
+        public Guid DisciplinaID { get; set; }
+        public Disciplina? Disciplina { get; set; }
+        
+        public double NotaCurricular { get; set; }
+    }
+}
