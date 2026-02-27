27735e4fb94b28a9f1f1b356d55392256918b243
Luan-Galera <luangalera17@gmail.com>
2026-02-27 14:36:48 -0300

    Cadastro de Cursos e relação AlunoCurso (N:M) adicionados
    Adicionados modelos Curso e AlunoCurso, controllers para CRUD, e migrations para novas tabelas e relações muitos-para-muitos. Atualizado AppDbContext e modelo Aluno para navegação. Iniciado modelo Disciplina.

---
+++ PrimeiraAPI/Models/AlunoCurso.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeiraAPI.Models
+{
+    public class AlunoCurso
+    {
+        // Identificador único do regitro da associação entre Aluno e Curso
+        public Guid AlunoCursoId { get; set; }
+
+        // Chave estrangeira para o Aluno
+        public Guid AlunoID { get; set; }
+        
+        // Propriedade de navegação para o Aluno
+        public Aluno? Aluno { get; set; }
+
+        // Chave estrangeira para o Curso
+        public Guid CursoID { get; set; }
+
+        // Propriedade de navegação para o Curso
+        public Curso? Curso { get; set; }
+    }
+}
---
+++ PrimeiraAPI/Models/Disciplina.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiraAPI.Models
+{
+    public class Disciplina
+    {
+    }
+}
